36da3d34e3ea01b525879df7d8b30fa0e35b5449
gabriel <gabriel@pop-os.localdomain>
2020-01-17 10:28:48 -0300

    Componentização e frontend web concluido.

--- frontend-web/src/App.js
+++ frontend-web/src/App.js
@@ -1,8 +1,11 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
+import api from './services/api';
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 
 
 
@@ -11,86 +14,38 @@ import './Main.css';
 // Estado: informações mantidas pelo componente (lembrar: imutabilidade)
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  //Comando faz com que carreguem os usuarios cadastrados apenas uma vez.
+  useEffect(() => {
+    async function loadDevs(){
+      const response = await api.get('/listarDevs');
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  },[]);
  
+  async function handleAddDev(data){
+    const response = await api.post('/addDevs', data);
+    console.log(response.data);
+
+    //Fazendo uma adição no array, para que os devs se atualizem automaticamente
+    //ao salvar
+    setDevs([...devs, response.data]);
+  }
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Longitude</label>
-              <input name="latitude" id="latitude" required/>
-            </div>
-            
-            <div className="input-block">
-              <label htmlFor="longitude">Latitude</label>
-              <input name="longitude" id="longitude" required/>
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/46577395?s=460&v=4" alt="Gabriel Barreto"/>
-              <div className="user-info">
-                <strong>Gabriel Barreto</strong>
-                <span>NodeJS, ReactJS</span>
-              </div>
-            </header>
-            <p>Estudante de Engenharia da computação apaixonado por programação e principalmente por desenvolvimento web</p>
-            <a href="https://github.com/GabrielDevLoper">Acessar Perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/46577395?s=460&v=4" alt="Gabriel Barreto"/>
-              <div className="user-info">
-                <strong>Gabriel Barreto</strong>
-                <span>NodeJS, ReactJS</span>
-              </div>
-            </header>
-            <p>Estudante de Engenharia da computação apaixonado por programação e principalmente por desenvolvimento web</p>
-            <a href="https://github.com/GabrielDevLoper">Acessar Perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/46577395?s=460&v=4" alt="Gabriel Barreto"/>
-              <div className="user-info">
-                <strong>Gabriel Barreto</strong>
-                <span>NodeJS, ReactJS</span>
-              </div>
-            </header>
-            <p>Estudante de Engenharia da computação apaixonado por programação e principalmente por desenvolvimento web</p>
-            <a href="https://github.com/GabrielDevLoper">Acessar Perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/46577395?s=460&v=4" alt="Gabriel Barreto"/>
-              <div className="user-info">
-                <strong>Gabriel Barreto</strong>
-                <span>NodeJS, ReactJS</span>
-              </div>
-            </header>
-            <p>Estudante de Engenharia da computação apaixonado por programação e principalmente por desenvolvimento web</p>
-            <a href="https://github.com/GabrielDevLoper">Acessar Perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
+         
         </ul>
       </main>
     </div>
